979ef131198b98cbdb7b2c138dd2106b295e6be6
Falase Femi <femifalase228@gmail.com>
2023-04-13 16:23:17 +0100

    Merged changes from remote repository

---
+++ schemas/product.js
@@ -0,0 +1,48 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+        validation: (Rule) => Rule.required(),
+      },
+      {
+        name: 'category',
+        title: 'Category',
+        type: 'string',
+        options: {
+          list: [
+            { title: 'Phone', value: 'phone' },
+            { title: 'Refrigerator', value: 'refrigerator' },
+            { title: 'Television', value: 'television' },
+            { title: 'Sneakers', value: 'sneakers' },
+          ],
+        },
+        validation: (Rule) => Rule.required(),
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+        validation: (Rule) => Rule.required(),
+      },
+      {
+        name: 'price',
+        title: 'Price',
+        type: 'number',
+        validation: (Rule) => Rule.required(),
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+      },
+    ],
+  };
+  
+  
+  
+  
